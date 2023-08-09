f947fdf27189f0ab30d4073adfc2748214b9955a
AnatoliiShara <anfrankleen23@gmail.com>
2023-08-09 18:14:37 +0300

    adding array partition solution

---
+++ array_partition.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def arrayPartition(nums: List[int]) -> int: 
+	nums.sort()
+	summa = 0 
+	for i in range(0, len(nums), 2):
+		summa += nums[i]
+	return summa
+print(arrayPartition([1,4,3,2]))
+
+
+
+
+
+
+
+
+
+
