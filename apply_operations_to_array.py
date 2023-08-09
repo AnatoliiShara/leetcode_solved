0cbaf024b147d05ec281859fa0f35c1c851323bf
AnatoliiShara <anfrankleen23@gmail.com>
2023-08-09 17:58:10 +0300

    adding apply operations to array solution

---
+++ apply_operations_to_array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def applyOperations(nums: List[int]) -> List[int]:
+	for i in range(len(nums) - 1):
+		if nums[i] == nums[i + 1]:
+			nums[i], nums[i + 1] = nums[i + 1] * 2, 0 
+	return sorted(nums, key=lambda x: x==0, reverse=False)
+print(applyOperations([1,2,2,2,1,1,0]))
+
+def applyOperations2(nums: List[int]) -> List[int]:
+	for i in range(len(nums) - 1):
+		if nums[i] == nums[i + 1]:
+			nums[i] *= 2 
+			nums[i + 1] = 0 
+	non_zeros = [x for x in nums if x != 0]
+	zeros = [x for x in nums if x == 0]
+	return non_zeros + zeros
+print(applyOperations2([1,2,2,2,1,1,0]))
+
