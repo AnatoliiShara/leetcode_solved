2b6d3d787c0e25b2586458cec1c9ce87a0331925
AnatoliiShara <anfrankleen23@gmail.com>
2023-08-09 18:31:03 +0300

    adding average value even numbers divided by 3

---
+++ average_value_even_numbers_divided_three.py
@@ -0,0 +1,15 @@
+import math 
+from typing import List
+
+def averageValue(nums: List[int]) -> int:
+	summa = 0 
+	count = 0
+	for num in nums:
+		if num % 6 == 0:
+			summa += num
+			count += 1
+	if count == 0:
+		return 0 
+	return math.floor(summa / count)
+print(averageValue([1,3,6,10,12,15]))
+
