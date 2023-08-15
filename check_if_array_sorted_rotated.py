9af301d00c50a85b361697726047330c61f71fd8
AnatoliiShara <anfrankleen23@gmail.com>
2023-08-15 12:20:47 +0300

    a few changes made

--- check_if_array_sorted_rotated.py
+++ check_if_array_sorted_rotated.py
@@ -11,3 +11,32 @@ def check(nums: List[int]) -> bool:
 nums = [3,4,5,1,2]
 res = check(nums)
 print(f"Rotation possible: {res}")
+
+"""
+
+"""
+
+def check300(nums: List[int]) -> bool:
+	n = len(nums)
+	# find index of the smallest element in array
+	smallest_idx = 0
+	for i in range(1, n):
+		if nums[i] < nums[smallest_idx]:
+			smallest_idx = i 
+
+	# check if array is sorted after rotation
+	for i in range(n):
+		original_idx = (i + smallest_idx) % n 
+		if nums[i] != nums[original_idx]:
+			return False
+	return True
+
+
+
+
+
+
+
+
+
+
